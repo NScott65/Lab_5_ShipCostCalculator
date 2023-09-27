bc30037a0d6616990e247549e0a44d8edbc51982
Noah <scott.613865@swl.k12.oh.us>
2023-09-27 09:43:34 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //Variables
+        double ogAmount = 0;
+        double shipping = 0;
+        double shipCost = 0;
+
+        System.out.print("Enter amount of purchase: ");
+        if(scan.hasNextDouble()){
+            ogAmount = scan.nextDouble();
+        }else if(ogAmount < 1){
+            System.out.print("Your input was invalid.");
+            System.exit(0);
+        }
+        shipping = ogAmount * .02;
+        shipCost = ogAmount + shipping;
+
+        if(ogAmount >= 100){
+            System.out.print("Your total cost is " + ogAmount + ".");
+            System.exit(0);
+        }else if(ogAmount < 100){
+            System.out.print("Your total cost after shipping is " + shipCost + ".");
+            System.exit(0);
+        }
     }
 }
